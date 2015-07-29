7c1bf2f5b0532ab37014d512cb8fd8cb86d1e8d9
Sumeru Chatterjee <sumeru.chatterjee@blippar.com>
2015-07-29 15:55:20 +0200

    Initial Commit

---
+++ ios/libTremelo/libTremelo/libTremelo.h
@@ -0,0 +1,13 @@
+//
+//  libTremelo.h
+//  libTremelo
+//
+//  Created by Sumeru Chatterjee on 7/29/15.
+//  Copyright (c) 2015 Blippar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libTremelo : NSObject
+
+@end
---
+++ ios/libTremelo/libTremelo/libTremelo.m
@@ -0,0 +1,13 @@
+//
+//  libTremelo.m
+//  libTremelo
+//
+//  Created by Sumeru Chatterjee on 7/29/15.
+//  Copyright (c) 2015 Blippar. All rights reserved.
+//
+
+#import "libTremelo.h"
+
+@implementation libTremelo
+
+@end
