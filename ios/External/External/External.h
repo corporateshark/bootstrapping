13850bda3ba56fcb12e6cf78af2cf5e0465788cf
Sumeru Chatterjee <sumeru.chatterjee@blippar.com>
2015-07-30 16:58:58 +0200

    Recreated External Project

---
+++ ios/External/External/External.h
@@ -0,0 +1,13 @@
+//
+//  External.h
+//  External
+//
+//  Created by Sumeru Chatterjee on 7/30/15.
+//  Copyright (c) 2015 Blippar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface External : NSObject
+
+@end
---
+++ ios/External/External/External.m
@@ -0,0 +1,13 @@
+//
+//  External.m
+//  External
+//
+//  Created by Sumeru Chatterjee on 7/30/15.
+//  Copyright (c) 2015 Blippar. All rights reserved.
+//
+
+#import "External.h"
+
+@implementation External
+
+@end
