c92a428eef59ea6b806e0674ffacdc5e9a22d846
Sumeru Chatterjee <sumeru.chatterjee@blippar.com>
2015-10-14 18:06:53 +0200

    Some work on building libjpeg turbo

--- ios/libjpeg-turbo/build.sh
+++ ios/libjpeg-turbo/build.sh
@@ -10,6 +10,9 @@ OUTPUTDIR="$( cd "$( dirname "$1" )" && pwd )"
 EXTSRCDIR=$DIR/../../src
 SRCDIR=$EXTSRCDIR/libjpeg-turbo
 
+echo "OUTPUTDIR=$OUTPUTDIR"
+echo "SRCDIR=$SRCDIR"
+
 # add gas-preprocessor to PATH
 PATH=$EXTSRCDIR/gas-preprocessor:$PATH
 
@@ -21,34 +24,51 @@ IPHONEOSVERSION="-miphoneos-version-min=5.1"
 
 # call autoreconf in source dir
 cd $SRCDIR
-autoreconf -fiv configure.ac
+autoreconf -fiv configure.ac 2> /dev/null
+if [[ $? -ne 0 ]] ; then
+    echo "Errors while running autoreconf. Exiting.."
+    exit 1
+fi
 
 CPUS=$(sysctl -n hw.logicalcpu_max)
 
 # build for ARMv7
 
-ARCH="armv7"
-BUILDDIR=$OUTPUTDIR/build_$ARCH
-INSTALLDIR=$OUTPUTDIR/install_$ARCH
-IOS_CFLAGS="-arch $ARCH $IPHONEOSVERSION"
+arch="armv7"
+ARCHS="armv7"
+BUILDDIR=$OUTPUTDIR/build_$arch
+INSTALLDIR=$OUTPUTDIR/install_$arch
+IOS_CFLAGS="-arch $arch $IPHONEOSVERSION"
 
 mkdir -p $BUILDDIR && cd $BUILDDIR
 
+printenv > /Users/sumeruchatterjee/Desktop/env.log
+
+SDKROOT="MacOS"
 $SRCDIR/configure --prefix=$INSTALLDIR --host arm-apple-darwin10 \
   CC="$IOS_GCC" LD="$IOS_GCC" \
   CFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT -O3 $IOS_CFLAGS" \
   LDFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT $IOS_CFLAGS" \
   CCASFLAGS="-no-integrated-as $IOS_CFLAGS"
+if [[ $? -ne 0 ]] ; then
+    echo "Errors while running configure. Exiting.."
+    exit 1
+fi
+SDKROOT="iPhoneOS"
 
 make -j$CPUS
 make install
+if [[ $? -ne 0 ]] ; then
+    echo "Errors while running make. Exiting.."
+    exit 1
+fi
 
 # build for ARMv7s
 
-ARCH="armv7s"
-BUILDDIR=$OUTPUTDIR/build_$ARCH
-INSTALLDIR=$OUTPUTDIR/install_$ARCH
-IOS_CFLAGS="-arch $ARCH $IPHONEOSVERSION"
+arch="armv7s"
+BUILDDIR=$OUTPUTDIR/build_$arch
+INSTALLDIR=$OUTPUTDIR/install_$arch
+IOS_CFLAGS="-arch $arch $IPHONEOSVERSION"
 
 mkdir -p $BUILDDIR && cd $BUILDDIR
 
@@ -57,16 +77,25 @@ $SRCDIR/configure --prefix=$INSTALLDIR --host arm-apple-darwin10 \
   CFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT -O3 $IOS_CFLAGS" \
   LDFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT $IOS_CFLAGS" \
   CCASFLAGS="-no-integrated-as $IOS_CFLAGS"
+if [[ $? -ne 0 ]] ; then
+    echo "Errors while running configure. Exiting.."
+    exit 1
+fi
 
 make -j$CPUS
 make install
+if [[ $? -ne 0 ]] ; then
+    echo "Errors while running make. Exiting.."
+    exit 1
+fi
+
 
 # build for ARMv8
 
-ARCH="arm64"
-BUILDDIR=$OUTPUTDIR/build_$ARCH
-INSTALLDIR=$OUTPUTDIR/install_$ARCH
-IOS_CFLAGS="-arch $ARCH $IPHONEOSVERSION"
+arch="arm64"
+BUILDDIR=$OUTPUTDIR/build_$arch
+INSTALLDIR=$OUTPUTDIR/install_$arch
+IOS_CFLAGS="-arch $arch $IPHONEOSVERSION"
 
 mkdir -p $BUILDDIR && cd $BUILDDIR
 
@@ -74,6 +103,14 @@ $SRCDIR/configure --prefix=$INSTALLDIR --host aarch64-apple-darwin \
   CC="$IOS_GCC" LD="$IOS_GCC" \
   CFLAGS="-isysroot $IOS_SYSROOT -O3 $IOS_CFLAGS" \
   LDFLAGS="-isysroot $IOS_SYSROOT $IOS_CFLAGS"
+if [[ $? -ne 0 ]] ; then
+    echo "Errors while running configure. Exiting.."
+    exit 1
+fi
 
 make -j$CPUS
 make install
+if [[ $? -ne 0 ]] ; then
+    echo "Errors while running make. Exiting.."
+    exit 1
+fi
