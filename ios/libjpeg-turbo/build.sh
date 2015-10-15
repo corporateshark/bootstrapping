b3e7d43938007308dae102af4cd2745d90d3e5b0
Sumeru Chatterjee <sumeru.chatterjee@blippar.com>
2015-10-15 16:25:11 +0200

    Fixed libjpeg-turbo Build Script

--- ios/libjpeg-turbo/build.sh
+++ ios/libjpeg-turbo/build.sh
@@ -1,17 +1,92 @@
-if [ $# -eq 0 ]
+if ! [ $# -eq 3 ]
   then
-    echo "ERROR: need to give output directory as first argument"
-    exit 0
+    echo "Usage: build.sh <build_directory> <install_directory> <action>"
+    exit 1
 fi
+
+if [ -z "$1" ]; then
+    echo "ERROR: Build Directory is not valid."
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "ERROR: Install Directory is not valid."
+    exit 1
+fi
+
+#Create build directory if not exists
+[ -d "$1" ] || mkdir -p "$1"
+
+#Create install directory if not exists
+[ -d "$2" ] || mkdir -p "$2"
+
+function clean(){
+  echo "Cleaning.."
+  if [ -d "$BUILDDIRECTORY" ]; then
+    cd $BUILDDIRECTORY
+    make distclean
+  fi
+  if [ -d "$INSTALLDIRECTORY" ]; then
+    rm -rf "$INSTALLDIRECTORY"
+  fi
+}
+
+function autoreconfigure(){
+  echo "AutoReconfiguring..."
+  autoreconf -fiv configure.ac 2> /dev/null
+  if [[ $? -ne 0 ]] ; then
+    echo "Errors while running autoreconf. Exiting.."
+    exit 1
+  fi
+}
+
+function configure(){
+  echo "Configuring..."
+   # We have to temporarily unset SDKROOT for configuration to go through
+  PREVIOUS_SDKROOT=$SDKROOT
+  SDKROOT=""
+  echo "Temporaily unsetting SDKROOT"
+
+  $SRCDIR/configure \
+    --prefix=$INSTALLDIRECTORY \
+    --host "$HOST" \
+    CC="$IOS_GCC" \
+    LD="$IOS_GCC" \
+    CFLAGS="$CFLAGS" \
+    LDFLAGS="$LDFLAGS" \
+    CCASFLAGS="$CCASFLAGS"
+  if [[ $? -ne 0 ]] ; then
+    echo "Errors while running configure. Exiting.."
+    exit 1
+  fi
+
+  echo "Setting SDKROOT Back to $PREVIOUS_SDKROOT"
+  SDKROOT=$PREVIOUS_SDKROOT
+}
+
+function build(){
+  echo "Building.."
+  make -j$CPUS
+  make install
+  if [[ $? -ne 0 ]] ; then
+      echo "Errors while running make. Exiting.."
+      exit 1
+  fi
+}
+
 
 # get some absolute paths
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-OUTPUTDIR="$( cd "$( dirname "$1" )" && pwd )"
+BUILDDIR="$( cd "$1" && pwd )"
+INSTALLDIR="$( cd "$2" && pwd )"
 EXTSRCDIR=$DIR/../../src
 SRCDIR=$EXTSRCDIR/libjpeg-turbo
+ACTION="$3"
 
-echo "OUTPUTDIR=$OUTPUTDIR"
+echo "BUILDDIR=$BUILDDIR"
+echo "INSTALLDIR=$INSTALLDIR"
 echo "SRCDIR=$SRCDIR"
+echo "ACTION=$ACTION"
 
 # add gas-preprocessor to PATH
 PATH=$EXTSRCDIR/gas-preprocessor:$PATH
@@ -21,96 +96,95 @@ IOS_PLATFORMDIR=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.pl
 IOS_SYSROOT=$IOS_PLATFORMDIR/Developer/SDKs/iPhoneOS.sdk
 IOS_GCC=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
 IPHONEOSVERSION="-miphoneos-version-min=5.1"
-
-# call autoreconf in source dir
-cd $SRCDIR
-autoreconf -fiv configure.ac 2> /dev/null
-if [[ $? -ne 0 ]] ; then
-    echo "Errors while running autoreconf. Exiting.."
-    exit 1
-fi
-
 CPUS=$(sysctl -n hw.logicalcpu_max)
+
+if [ "$ACTION" == "build" ];
+then
+  # call autoreconf in source dir
+  cd $SRCDIR
+  autoreconfigure
+fi
 
 # build for ARMv7
 
 arch="armv7"
-ARCHS="armv7"
-BUILDDIR=$OUTPUTDIR/build_$arch
-INSTALLDIR=$OUTPUTDIR/install_$arch
+BUILDDIRECTORY=$BUILDDIR/$arch
+INSTALLDIRECTORY=$INSTALLDIR/$arch
 IOS_CFLAGS="-arch $arch $IPHONEOSVERSION"
+HOST="arm-apple-darwin10"
+CFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT -O3 $IOS_CFLAGS"
+LDFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT $IOS_CFLAGS"
+CCASFLAGS="-no-integrated-as $IOS_CFLAGS"
 
-mkdir -p $BUILDDIR && cd $BUILDDIR
-
-printenv > /Users/sumeruchatterjee/Desktop/env.log
-
-SDKROOT="MacOS"
-$SRCDIR/configure --prefix=$INSTALLDIR --host arm-apple-darwin10 \
-  CC="$IOS_GCC" LD="$IOS_GCC" \
-  CFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT -O3 $IOS_CFLAGS" \
-  LDFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT $IOS_CFLAGS" \
-  CCASFLAGS="-no-integrated-as $IOS_CFLAGS"
-if [[ $? -ne 0 ]] ; then
-    echo "Errors while running configure. Exiting.."
-    exit 1
-fi
-SDKROOT="iPhoneOS"
-
-make -j$CPUS
-make install
-if [[ $? -ne 0 ]] ; then
-    echo "Errors while running make. Exiting.."
-    exit 1
+if [ "$ACTION" == "clean" ]
+then
+  clean
+elif [ "$ACTION" == "build" ]
+then
+  mkdir -p $BUILDDIRECTORY && cd $BUILDDIRECTORY 
+  configure
+  build
+else
+  echo "Unrecognized Action: $ACTION"
 fi
 
 # build for ARMv7s
 
 arch="armv7s"
-BUILDDIR=$OUTPUTDIR/build_$arch
-INSTALLDIR=$OUTPUTDIR/install_$arch
+BUILDDIRECTORY=$BUILDDIR/$arch
+INSTALLDIRECTORY=$INSTALLDIR/$arch
 IOS_CFLAGS="-arch $arch $IPHONEOSVERSION"
+HOST="arm-apple-darwin10"
+CFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT -O3 $IOS_CFLAGS"
+LDFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT $IOS_CFLAGS"
+CCASFLAGS="-no-integrated-as $IOS_CFLAGS"
 
-mkdir -p $BUILDDIR && cd $BUILDDIR
-
-$SRCDIR/configure --prefix=$INSTALLDIR --host arm-apple-darwin10 \
-  CC="$IOS_GCC" LD="$IOS_GCC" \
-  CFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT -O3 $IOS_CFLAGS" \
-  LDFLAGS="-mfloat-abi=softfp -isysroot $IOS_SYSROOT $IOS_CFLAGS" \
-  CCASFLAGS="-no-integrated-as $IOS_CFLAGS"
-if [[ $? -ne 0 ]] ; then
-    echo "Errors while running configure. Exiting.."
-    exit 1
+if [ "$ACTION" == "clean" ]
+then
+  clean
+elif [ "$ACTION" == "build" ]
+then
+  mkdir -p $BUILDDIRECTORY && cd $BUILDDIRECTORY
+  configure
+  build
+else
+  echo "Unrecognized Action: $ACTION"
 fi
-
-make -j$CPUS
-make install
-if [[ $? -ne 0 ]] ; then
-    echo "Errors while running make. Exiting.."
-    exit 1
-fi
-
 
 # build for ARMv8
 
 arch="arm64"
-BUILDDIR=$OUTPUTDIR/build_$arch
-INSTALLDIR=$OUTPUTDIR/install_$arch
+BUILDDIRECTORY=$BUILDDIR/$arch
+INSTALLDIRECTORY=$INSTALLDIR/$arch
 IOS_CFLAGS="-arch $arch $IPHONEOSVERSION"
+HOST="aarch64-apple-darwin"
+CFLAGS="-isysroot $IOS_SYSROOT -O3 $IOS_CFLAGS" 
+LDFLAGS="-isysroot $IOS_SYSROOT $IOS_CFLAGS"
+CCASFLAGS=""
 
-mkdir -p $BUILDDIR && cd $BUILDDIR
-
-$SRCDIR/configure --prefix=$INSTALLDIR --host aarch64-apple-darwin \
-  CC="$IOS_GCC" LD="$IOS_GCC" \
-  CFLAGS="-isysroot $IOS_SYSROOT -O3 $IOS_CFLAGS" \
-  LDFLAGS="-isysroot $IOS_SYSROOT $IOS_CFLAGS"
-if [[ $? -ne 0 ]] ; then
-    echo "Errors while running configure. Exiting.."
-    exit 1
+if [ "$ACTION" == "clean" ]
+then
+  clean
+elif [ "$ACTION" == "build" ]
+then
+  mkdir -p $BUILDDIRECTORY && cd $BUILDDIRECTORY
+  configure
+  build
+else
+  echo "Unrecognized Action: $ACTION"
 fi
 
-make -j$CPUS
-make install
-if [[ $? -ne 0 ]] ; then
-    echo "Errors while running make. Exiting.."
-    exit 1
+# Combine the binaries
+
+if [ "$ACTION" == "clean" ]
+then
+  echo "Removing Universal Binaries"
+  rm -f $INSTALLDIR/*.a
+elif [ "$ACTION" == "build" ]
+then
+  echo "Creating Universal Binary"
+  (cd $INSTALLDIR && lipo -create arm64/lib/libturbojpeg.a armv7/lib/libturbojpeg.a armv7s/lib/libturbojpeg.a -o libjpegturbo.a)
+  echo "BUILT PRODUCT: $INSTALLDIR/libturbojpeg.a"
+else
+  echo "Unrecognized Action: $ACTION"
 fi
--- ios/libjpeg-turbo/xcodebuild.sh
+++ ios/libjpeg-turbo/xcodebuild.sh
@@ -2,11 +2,15 @@
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+ACTION=$1
+
 [ -z "$PROJECT_DIR" ] && PROJECT_DIR=$SCRIPT_DIR
-[ -z "$OBJROOT" ] && OBJROOT=$SCRIPT_DIR
+[ -z "$OBJROOT" ] && OBJROOT=$SCRIPT_DIR/build
+[ -z "$SYMROOT" ] && SYMROOT=$SCRIPT_DIR/install
+[ -z "$ACTION" ] && ACTION="build"
 
-echo "Running build script with $OBJROOT as OBJROOT"
+echo "Running build script with $OBJROOT as OBJROOT and $SYMROOT as SYMROOT and $ACTION as ACTION"
 
-"$PROJECT_DIR"/build.sh "$OBJROOT"
+"$PROJECT_DIR"/build.sh "$OBJROOT" "$SYMROOT" "$ACTION"
 
 
