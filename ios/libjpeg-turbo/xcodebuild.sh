421f08c2c3da6b7d6abaadbd358223567002e50a
Sumeru Chatterjee <sumeru.chatterjee@blippar.com>
2015-10-19 14:25:11 +0100

    fix libjpeg-turbo xcodebuild for command line builds (FAST-DEBUG)

--- ios/libjpeg-turbo/xcodebuild.sh
+++ ios/libjpeg-turbo/xcodebuild.sh
@@ -13,6 +13,21 @@ ACTION=$1
 
 "$PROJECT_DIR"/build.sh "$CONFIGURATION_TEMP_DIR" "$CONFIGURATION_BUILD_DIR" "$ACTION" "$PRODUCT_NAME" "$CONFIGURATION"
 
+if [[ "$CONFIGURATION_BUILD_DIR" == "$PROJECT_DIR"* ]]
+then
+  # If the build is inside the source directory then create symbolic link in root external repository to make linking work
 
+  EXTERNAL_PROJECT_DIR="$PROJECT_DIR/../.."
+  EXTERNAL_PROJECT_BUILD_DIR="$EXTERNAL_PROJECT_DIR/build/$CONFIGURATION-iphoneos"
+    
+  if [ -h "$EXTERNAL_PROJECT_BUILD_DIR/$PRODUCT_NAME".a ] ; then
+    echo "Removing existing symbolic link at $EXTERNAL_PROJECT_BUILD_DIR/$PRODUCT_NAME.a"
+    rm "$EXTERNAL_PROJECT_BUILD_DIR/$PRODUCT_NAME".a
+  fi
 
+  set -x
+  echo "Creating symbolic link at $EXTERNAL_PROJECT_BUILD_DIR/$PRODUCT_NAME".a
+  ln -s "$CONFIGURATION_BUILD_DIR/$PRODUCT_NAME".a "$EXTERNAL_PROJECT_BUILD_DIR/$PRODUCT_NAME".a
+  set +x
+fi
 
